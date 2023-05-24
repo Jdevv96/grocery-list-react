5c7ded9070186a2ed2ca383398d28bea73ab1648
Josue_Tejada <omar14tejada@gmail.com>
2023-05-23 23:11:42 -0400

    Initial file commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import Content from "./Content";
+import AddItem from "./AddItem";
+import Footer from "./Footer";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "Bread",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Eggs",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Bananas",
+    },
+  ]);
+
+  const [newItem, setNewItem] = useState("");
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
+  }
+
+  const addItem = (item) => {
+    const id = item.length ? items[items.length-1].id + 1 : 1;
+    const addedItem = {id, checked: false, item};
+    const listItems = [...items, addedItem];
+    setAndSaveItems(listItems);
+  }
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setAndSaveItems(listItems);
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setAndSaveItems(listItems);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!newItem) return;
+    //add item
+    addItem(newItem);
+    setNewItem('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
     </div>
   );
 }
